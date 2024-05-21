2ed049c660107e8348f4152dd74de8b29b921180
TCWaterhouse <thomas_waterhouse@hotmail.co.uk>
2024-05-21 04:56:40 +0100

    updated gitignore

--- src/main.py
+++ src/main.py
@@ -1,23 +1,7 @@
-from textnode import (
-    TextNode, 
-    text_type_text, 
-    text_type_bold, 
-    text_type_italic, 
-    text_type_code
-)
 
-from htmlnode import (
-    HTMLNode, 
-    LeafNode, 
-    ParentNode
-)
-
-from inline_markdown import split_nodes_delimiter
 
 def main():
-    node = TextNode("This is text with a `code block` word", text_type_text)
-    new_nodes = split_nodes_delimiter([node], "`", text_type_code)
-    print(len(new_nodes))
+    pass
     
 if __name__ == "__main__":
     main()
