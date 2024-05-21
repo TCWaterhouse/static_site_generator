5a9637603008ed73159c353ccb211e5d4a45fd34
TCWaterhouse <thomas_waterhouse@hotmail.co.uk>
2024-05-21 21:06:57 +0100

    started func to copy dir

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
+import shutil
+import os
 
+def copy_dir(source, target):
+    # Clear target directory
+    shutil.rmtree(target)
 
 def main():
-    pass
+    copy_dir("static/", "public/")
     
 if __name__ == "__main__":
     main()
