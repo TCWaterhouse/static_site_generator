c5c74c6a6b0441b69c39f605c2e2da5a377204bb
TCWaterhouse <thomas_waterhouse@hotmail.co.uk>
2024-05-21 23:17:04 +0100

    finished copy files func

--- src/main.py
+++ src/main.py
@@ -1,12 +1,20 @@
-import shutil
 import os
+import shutil
 
-def copy_dir(source, target):
-    # Clear target directory
-    shutil.rmtree(target)
+from copystatic import copy_files_recursive
+
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+
 
 def main():
-    copy_dir("static/", "public/")
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
     
 if __name__ == "__main__":
     main()
